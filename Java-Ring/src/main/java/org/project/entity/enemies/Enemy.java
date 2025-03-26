e8fd12c04800361ab4641b395e8b40007a1b2e7c
AmirHossein <goodarzizadeh.ah@gmail.com>
2025-03-26 05:43:46 +0330

    update folder (Enemy): implementing methods

--- Java-Ring/src/main/java/org/project/entity/enemies/Enemy.java
+++ Java-Ring/src/main/java/org/project/entity/enemies/Enemy.java
@@ -1,16 +1,25 @@
 package org.project.entity.enemies;
 
-import org.project.object.weapons.Weapon;
+import org.project.entity.Entity;
+import org.project.object.weapons.*;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 // TODO: UPDATE IMPLEMENTATION
-public abstract class Enemy {
+public abstract class Enemy implements Entity{
     Weapon weapon;
     private int hp;
     private int mp;
 
+    private int maxHP;
+    private int maxMP;
+
     public Enemy(int hp, int mp, Weapon weapon) {
-        this.hp = hp;
-        this.mp = mp;
+        this.maxHP = hp;
+        this.maxMP = mp;
+        this.hp = maxHP;
+        this.mp = maxMP;
 
         this.weapon = weapon;
     }
@@ -21,10 +30,31 @@ public abstract class Enemy {
         hp -= damage;
     }
 
-    public int getHp() {
-        return hp;
+    public void attack(Entity target) {
+
+        if (this.mp - weapon.getManaCost() < 0) {
+            System.out.println(toString() + " ran out of mana ");
+        } else {
+            target.takeDamage(weapon.getDamage());
+            this.mp-= weapon.getManaCost();
+            System.out.println(toString() + " damaged " + target.toString()  + " remaining health "+ target.getHp()) ;
+        }
+
     }
 
+    @Override
+    public void attack(Entity target, ArrayList<Enemy> targets) {    }
+
+    public int getHp() {
+        if (hp >= 0)
+            return hp;
+        return 0;
+    }
+
+    public int getHp(boolean isdead) {
+            return hp;
+
+    }
     public int getMp() {
         return mp;
     }
@@ -32,4 +62,59 @@ public abstract class Enemy {
     public Weapon getWeapon() {
         return weapon;
     }
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void defend(){
+        // TODO: (BONUS) IMPLEMENT A DEFENSE METHOD FOR SHIELDS
+
+
+    }
+    public void heal(int health){
+        hp += health;
+        if (hp > maxHP) {
+            hp = maxHP;
+        }
+    }
+
+    public void fillMana(int mana){
+        mp += mana;
+        if (mp > maxMP) {
+            mp = maxMP;
+        }
+    }
+
+    public int getMaxHP(){return maxHP;}
+
+    public int getMaxMP(){return maxMP;}
+
+
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Enemy default";
+    }
+
+    protected static Weapon randomweapon(int damage , int manaCost) {
+        Random r = new Random();
+        int weaponsNumber = 4;
+        int random = r.nextInt(weaponsNumber);
+        switch (random) {
+            case 0:
+                return  new Mele(damage, manaCost);
+            case 1:
+                return  new Dager(damage, manaCost);
+            case 2:
+                return  new Sword(damage, manaCost);
+            case 3:
+                return  new Wand(damage, manaCost);
+            default:
+                return null;
+        }
+    }
+
 }
--- Java-Ring/src/main/java/org/project/entity/enemies/Goblin.java
+++ Java-Ring/src/main/java/org/project/entity/enemies/Goblin.java
@@ -1,4 +1,29 @@
 package org.project.entity.enemies;
 
-public class Goblin {
+import org.project.entity.Entity;
+import org.project.object.weapons.Dager;
+import org.project.object.weapons.Mele;
+import org.project.object.weapons.Weapon;
+
+import javax.print.attribute.standard.PrinterMakeAndModel;
+
+public class Goblin extends Enemy{
+
+    int manaRegeneration = 3;
+
+    public Goblin() {
+//        super(10,5, new Mele(1,1));
+        super(10,7, new Dager(2,6));
+    }
+
+    @Override
+    public String toString() {
+        return "Goblin";
+    }
+
+    @Override
+    public void attack(Entity target) {
+        super.attack(target);
+        super.fillMana(manaRegeneration);
+    }
 }
