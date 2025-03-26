0d05b77751e86325b356f560d0600a89fd8f982e
AmirHossein <goodarzizadeh.ah@gmail.com>
2025-03-26 05:58:51 +0330

    update folder (consumables): add more potions

--- Java-Ring/src/main/java/org/project/object/consumables/Consumable.java
+++ Java-Ring/src/main/java/org/project/object/consumables/Consumable.java
@@ -1,8 +1,22 @@
 package org.project.object.consumables;
 
+import org.project.entity.Entity;
+import org.project.object.Object;
+
 // TODO: UPDATE IMPLEMENTATION
-public abstract class Consumable {
+public abstract class Consumable implements Object {
+
+
+    public void use(Entity target) {}
     /*
     TODO: ADD OTHER REQUIRED AND BONUS METHODS
     */
+
+    @Override
+    public String toString() {
+        return "consumable";
+    }
+
+    public int getRemainings(){System.out.println("wrong consumable object call"); return 0;}
+    public void resetRemainings(){}
 }
--- Java-Ring/src/main/java/org/project/object/consumables/Flask.java
+++ Java-Ring/src/main/java/org/project/object/consumables/Flask.java
@@ -3,14 +3,30 @@ package org.project.object.consumables;
 import org.project.entity.Entity;
 
 // TODO: UPDATE IMPLEMENTATION
-public class Flask {
+public class Flask extends Consumable {
     /*
     THIS IS AN EXAMPLE OF A CONSUMABLE DESIGN.
     */
+    private int remainingFlasks = 5;
+
 
     // TODO: (BONUS) UPDATE USE METHOD
     @Override
     public void use(Entity target) {
+        if (remainingFlasks > 0) {
+        remainingFlasks--;
         target.heal(target.getMaxHP() / 10);
+    } else {
+            System.out.println("« no more flasks!! »");
+        }
+    }
+    @Override
+    public int getRemainings() {return remainingFlasks; }
+    @Override
+    public void resetRemainings() {remainingFlasks = 5; }
+
+    @Override
+    public String toString() {
+        return "flask";
     }
 }
